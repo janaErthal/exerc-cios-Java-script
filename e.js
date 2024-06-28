77077bb64594734da87f11e8a1ed7b2be1d508d1
janaina <janaina.erthal@gmail.com>
2024-06-27 21:42:47 -0300

    feito 1 ate 8(metade)

---
+++ e.js
@@ -0,0 +1,79 @@
+// Ex.01 vari
+let nome = "joão"
+console.log(nome)
+
+
+//Ex.02 vari
+let soma, a = 10 , b = 5
+console.log(soma)
+soma = a + b
+
+//Ex.03 vari
+   let primeiroNome = "joseva"  , sobrenome = "silva"
+   console.log(primeiroNome,sobrenome)
+
+   
+   //Ex.04 vari
+   let pessoa = {
+    nome : "jana",
+    idade : 17,
+    telefone: 888-888,
+    endereco: "jana@gmail88"
+   }
+
+   //Ex.04 vt
+   let frutas = ["maçã" , "banana" , "açai"]
+   frutas[3] = "pera"
+   console.log(frutas)
+   frutas.push("abacate")
+   console.log(frutas)
+   frutas[0] = "abacaxi"
+   console.log(frutas)
+
+      console.log("o tamanho do vetor é: ", frutas.length)
+
+   for(let i =0;i<5;i++){
+      console.log(frutas[i])
+
+   }
+
+   //Ex.05 vt
+    let numeros = [1,2,3,4,5]
+    console.log(numeros[0],numeros[4]);
+
+    //Ex.06 vt
+    let cores = ["vermelho","verde","azul"]
+    console.log(cores)
+
+    for(let i =0;i<2;i++){
+        console.log(cores[i])
+        console.log("cores:",cores)
+    }
+
+    //Ex.07 Es.D
+    let idade = 18
+      if(idade >=18){
+        console.log("maior idade",idade)
+      }else{
+        console.log("menor de idade",idade)
+      }
+
+      //Ex.08 Es.C
+      let nota
+      console.log("sua nota:",nota)
+       if(nota > 7 ) {
+        console.log("aprovado");
+        
+       }
+
+    
+
+
+
+
+
+
+
+
+
+
